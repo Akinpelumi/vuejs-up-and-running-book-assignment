cd78b036f293a7c087f92acf0600605fb772491e
Akinpelumi <54557636+Akinpelumi@users.noreply.github.com>
2019-11-18 06:21:04 +0100

    Add files via upload

---
+++ chapter 7/src/components/HelloWorld.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="hello">
+    <p>Messages: {{ messageCount }}</p>
+    <!-- <p>Messages: {{ messages.length }}<a @click.prevent="handleUpdate">(update)</a></p>
+    <form @submit="handleSubmit">...</form> -->
+  </div>
+</template>
+
+
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+import { mount } from 'vue-test-utils'
+
+const wrapper = mount(NotificationCount, initialCount)
+const count = wrapper.find('.count').text();
+  expect(count).toBe('5')
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      formData: {}
+    }
+  },
+  propsData: {
+  initialCount: 5
+  },
+  computed: mapState({
+    messageCount: (state) => state.messageCount,
+    }),
+  ...mapGetters({
+    unreadMessages: 'unread',
+    unreadMessagesFrom: 'unreadFrom'
+    }),
+  mounted() {
+    this.$store.dispatch('getMessages');
+  },
+  methods: {
+    handleSubmit() {
+    this.$store.commit('addMessage', this.formData);
+    },
+    handleUpdate() {
+    this.$store.dispatch('getMessages');
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
